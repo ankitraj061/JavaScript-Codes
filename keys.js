8b24e8192b25375ffd0d65fc63ad805939ff7bd4
ankitraj061 <ankitraj07032002@gmail.com>
2024-01-03 00:14:39 +0530

    Another change

---
+++ keys.js
@@ -0,0 +1,24 @@
+let emp={
+    name: "Ankit",
+    rollNo: 20,
+    section:'A'
+}
+
+let keys=Object.keys(emp).forEach((key)=> console.log(key))
+//console.log(keys)
+
+
+let emp1={
+    name: "Ankit",
+    rollNo: 20,
+    section:'A'
+}
+console.log(Object.values(emp1))
+
+let emp2={
+    name: "Ankit",
+    rollNo: 20,
+    section:'A'
+}
+console.log(Object.entries(emp2))
+
